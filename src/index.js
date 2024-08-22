32a035fd46f5931ed83ed2a64bbf9d9e4afebb7a
tharan26 <tharan0026@gmail.com>
2024-08-22 16:41:57 +0530

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ShoppingCart from './ShoppingCart';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ShoppingCart />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
